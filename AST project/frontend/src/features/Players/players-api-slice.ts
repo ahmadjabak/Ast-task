7f4ec531cf2667ddca910ee75ed60a2d1e8609fc
ahmadjabak <ahmadjabak5@gmail.com>
2023-02-17 19:36:07 +0200

    mobile app

--- AST project/frontend/src/features/Players/players-api-slice.ts
+++ AST project/frontend/src/features/Players/players-api-slice.ts
@@ -6,15 +6,6 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
     reducerPath: 'Players',
     baseQuery: fetchBaseQuery({
         baseUrl:'https://63ece97e32a0811723a4ca01.mockapi.io/api/test',
-        prepareHeaders(headers){
-            // headers.set(
-            //     'X-RapidAPI-Key',FOOTBALL_API_KEY
-
-              
-            //     );
-              
-            // return headers;
-        }
     }),
     endpoints:(builder)=> ({
         
